de3fecd35a6aad5d0712ecf33d3bea43dedd5949
lfzm <510415008@qq.com>
2019-06-05 15:31:03 +0800

    add: addclient

--- src/Ocelot.OrleansHttpGateway/Configuration/OrleansHttpGatewayOptions.cs
+++ src/Ocelot.OrleansHttpGateway/Configuration/OrleansHttpGatewayOptions.cs
@@ -2,6 +2,7 @@
 using Orleans.Runtime;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Ocelot.OrleansHttpGateway.Configuration
 {
@@ -11,6 +12,23 @@ namespace Ocelot.OrleansHttpGateway.Configuration
 
         public int InitializeAttemptsBeforeFailing { get; set; } = 10;
 
+        /// <summary>
+        /// add client
+        /// </summary>
+        /// <param name="serviceName">serviceName</param>
+        /// <param name="serviceId">serviceId</param>
+        /// <param name="clusterId">clusterId</param>
+        /// <param name="assembly">clusterId</param>
+        public void AddClient(string serviceName,string serviceId,string clusterId, Assembly assembly)
+        {
+            Clients.Add(serviceName, new OrleansClientOptions
+            {
+                Assembly = assembly,
+                ClusterId = clusterId,
+                ServiceName=serviceName,
+                ServiceId=serviceId
+            });
+        }
     }
 
 
